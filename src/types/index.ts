0ef56cb2faf78453f2fc0dfa29cf623393e959b0
Yo Kobayashi <82624334+KobayashiYoh@users.noreply.github.com>
2025-10-12 14:30:06 +0900

    types/index.tsを追加してBarrel Fileパターンを導入

---
+++ src/types/index.ts
@@ -0,0 +1,4 @@
+export type { PieceType, Piece } from './piece';
+export type { BoardIndex, Position } from './position';
+export type { GameResult } from './gameResult';
+export type { Board } from './board';
