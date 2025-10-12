3b17b4716c04c02860b77d9757e666f482d132de
Yo Kobayashi <82624334+KobayashiYoh@users.noreply.github.com>
2025-10-13 01:02:34 +0900

    constants/index.tsのクォートをダブルクォートに統一

--- packages/shogi-core/src/constants/index.ts
+++ packages/shogi-core/src/constants/index.ts
@@ -1,3 +1,3 @@
-export * from './directions';
-export * from './gameText';
-export * from './initialBoard';
+export * from "./directions";
+export * from "./gameText";
+export * from "./initialBoard";
