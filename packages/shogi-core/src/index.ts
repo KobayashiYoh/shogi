2ddc8f581fe131236c4cb2d7c6cea5e8639f1ac4
Yo Kobayashi <82624334+KobayashiYoh@users.noreply.github.com>
2025-10-13 01:01:47 +0900

    GameMode型をshogi-coreパッケージからエクスポート

--- packages/shogi-core/src/index.ts
+++ packages/shogi-core/src/index.ts
@@ -1,11 +1,8 @@
 // Types
-export type { PieceType, Piece } from './types/piece';
-export type { BoardIndex, Position } from './types/position';
-export type { GameResult } from './types/gameResult';
-export type { Board } from './types/board';
+export * from "./types";
 
 // Constants
-export * from './constants';
+export * from "./constants";
 
 // Utils
-export * from './utils';
+export * from "./utils";
