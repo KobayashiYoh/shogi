db40f1fd444b057863a5df7914cd5ba1ce182468
Yo Kobayashi <82624334+KobayashiYoh@users.noreply.github.com>
2025-10-19 19:22:50 +0900

    packages/figjam-widget/src/components/index.tsを追加

---
+++ packages/figjam-widget/src/components/index.ts
@@ -0,0 +1,5 @@
+export { CapturedPiecesDisplay } from './CapturedPiecesDisplay';
+export { ContinuePage } from './ContinuePage';
+export { ModeSelectionPage } from './ModeSelectionPage';
+export { PieceDisplay } from './PieceDisplay';
+export { PlayingPage } from './PlayingPage';
